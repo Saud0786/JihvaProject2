db79fd437e3489d40b495b321ee52e3b7d04e721
Saud0786 <saud31253@gmail.com>
2025-05-02 15:27:35 +0530

    four

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,10 @@ import assets from '../../assets/assets'
 const Navbar = () => {
   return (
     <div className='navbar'>
-     <img className='logo' src={assets.logo} alt=""/>
+      <div>
+        <img className='logo' src={assets.logo} alt=""/>
+        <p>Admin Panel</p>
+      </div>
      <img className='profile' src={assets.profile_image} alt=""/>
     </div>
   )
